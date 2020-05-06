eea1ef5490fed4acc007f2c97bf6ea347c9b8426
Ibrahim Yahya <yahyalito1@gmail.com>
2020-05-06 12:36:24 +0300

    akan conditional statements

--- js/script.js
+++ js/script.js
@@ -3,18 +3,18 @@ function validateInput(){
   var month = Number(document.getElementById("month").value);
   var year = Number(document.getElementById("year").value);
   var century = Number(document.getElementById("century").value);
-  var genderSelected = document.getElementsByName("gender");
+  var genderSelect = document.getElementsByName("gender");
 
 
 
   function genderPicker() {
-    for (var gender of genderSelected) {
+    for (var gender of genderSelect) {
       if (gender.checked){
         return gender.value;
       }
     }
   }
-  var genderValue = genderPicker();
+  var myGender = genderPicker();
 
   function ValidateDay () {
     if (day < 0 || day > 31) {
@@ -39,12 +39,74 @@ function validateInput(){
   var dayOfWeek = Math.round(( ( (century/4) -2*century-1) + ((5*year/4) ) + ((26*(month+1)/10)) + day) % 7);
 
 
-
-
-
-
-
-
-
-
+  var akanName;
+  if (myGender === "male") {
+    switch(dayOfWeek) {
+      case 0:
+      akanName = "Kwasi";
+      alert("Your Akan name is"+akanName + "born on Sunday");
+      break;
+      case 1:
+      akanName = "Kwadwo";
+      alert("Your Akan name is"+akanName + "born on Monday");
+      break;
+      case 2:
+      akanName = "Kwabena";
+      alert("Your Akan name is"+akanName + "born on Tuesday");
+      break;
+      case 3:
+      akanName = "Kwaku";
+      alert("Your Akan name is"+akanName + "born on Wednesday");
+      break;
+      case 4:
+      akanName = "Yaw";
+      alert("Your Akan name is"+akanName + "born on Thursday");
+      break;
+      case 5:
+      akanName = "Kofi";
+      alert("Your Akan name is"+akanName + "born on Friday");
+      break;
+      case 6:
+      akanName = "Kwame";
+      alert("Your Akan name is"+akanName + "born on Saturday");
+      break;
+      default:
+      alert("");
+    }
+  } else if (myGender = "female") {
+    switch(dayOfWeek) {
+      case 0:
+      akanName = "Akosua";
+      alert("Your Akan name is"+akanName + "born on Sunday");
+      break;
+      case 1:
+      akanName = "Adwoa";
+      alert("Your Akan name is"+akanName + "born on Monday.");
+      break;
+      case 2:
+      akanName = "Abenaa";
+      alert("Your Akan name is"+akanName + "born on Tuesday");
+      break;
+      case 3:
+      akanName = "Akua";
+      alert("Your Akan name is"+akanName + "born on Wednesday");
+      break;
+      case 4:
+      akanName = "Yaa";
+      alert("Your Akan name is"+akanName + "born on Thursday");
+      break;
+      case 5:
+      akanName = "Afua";
+      alert("Your Akan name is"+akanName + "born on Friday");
+      break;
+      case 6:
+      akanName = "Ama";
+      alert("Your Akan name is"+akanName + "born on Saturday");
+      break;
+      default:
+      alert("");
+    }
+  } else {
+    alert("");
+  }
 }
