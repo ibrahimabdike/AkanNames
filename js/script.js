b7410f426f046a3ae6f908105055c92877399f9d
Ibrahim Yahya <yahyalito1@gmail.com>
2020-05-06 08:53:32 +0300

    main function,validateinput function

---
+++ js/script.js
@@ -0,0 +1,7 @@
+function validateInput(){
+  var day = Number(document.getElementById("day").value);
+  var month = Number(document.getElementById("month").value);
+  var year = Number(document.getElementById("year").value);
+  var century = Number(document.getElementById("century").value);
+  var genderSelected = document.getElementsByName("gender");
+}
